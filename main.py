0aff59f99eeb909ad7699f3f81f80d72d48d8507
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-10-07 21:29:06 -0500

    added error handling

--- main.py
+++ main.py
@@ -9,7 +9,24 @@ nato_alphabet_dict = {
 
 print(nato_alphabet_dict)
 
-word = input("Enter a word: ")
-letters = [x.capitalize() for x in word]
-code = [nato_alphabet_dict[letter] for letter in letters if letter in nato_alphabet_dict]
-print(code)
+def generate_phonetic():
+
+  word = input("Enter a word: ")
+  letters = [x.capitalize() for x in word]
+  try:
+    code = [nato_alphabet_dict[letter] for letter in letters if letter in nato_alphabet_dict]
+  except KeyError:
+    print('Sorry only letters in the alphabet')
+    generate_phonetic()
+  else:
+    print(code)
+generate_phonetic()
+
+
+
+# import tkinter
+
+# window = tkinter.Tk()
+# my_label = tkinter.Label(text="Hi how are you")
+# my_label.pack()
+# window.mainloop()
